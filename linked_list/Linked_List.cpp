379aaec8ba8e518095e89bacba44d4c3c082b0dc
PranavNaik16 <pranavnaik355@gmail.com>
2026-06-09 17:19:06 +0530

    Added new files

--- linked_list/Linked_List.cpp
+++ linked_list/Linked_List.cpp
@@ -15,12 +15,48 @@ class Node {
 
 };
 
-void InsertATHead(Node* &head, int d) {
+void InsertAtHead(Node* &head, int d) {
 
     // new node create
     Node* temp = new Node(d);
     temp -> next = head;
     head = temp;
+}
+
+void InsertAtTail(Node* &tail, int d) {
+
+    // new node create
+    Node* temp = new Node(d);
+    tail -> next = temp;
+    tail = tail -> next;
+}
+
+void InsertAtPosition(Node* &head, Node* &tail, int position, int d){
+
+    // Insert At Start
+    if(position == 1) {
+        InsertAtHead(head, d);
+        return;
+    }
+
+    Node* temp = head;
+    int cnt = 1;
+    while(cnt < position - 1 && temp != NULL){
+    temp = temp -> next;
+    cnt++;
+}
+
+    // Insert at end 
+    if(temp == tail) {
+        InsertAtTail(tail, d);
+        return;
+    }
+
+    // Create new Node
+    Node* nodeToInsert = new Node(d);
+
+    nodeToInsert -> next = temp-> next;
+    temp->next = nodeToInsert;
 }
 
 void print(Node* &head){
@@ -38,16 +74,30 @@ int main() {
 
     // Created a New Node
     Node* node1 = new Node(10);
-    cout << node1 -> data << endl;
-    cout << node1 -> next << endl;
+    // cout << node1 -> data << endl;
+    // cout << node1 -> next << endl;
 
-    // Head pointed to node1
+
+    Node *tail = node1;
     Node *head = node1;
+
+    // print(head);
+
+    InsertAtTail(tail, 12);
+    // print(head);
+
+    InsertAtTail(tail, 15);
+    // print(head);
+
+    InsertAtPosition(head, tail, 2, 11);
     print(head);
 
-    InsertATHead(head, 12);
-
+    InsertAtPosition(head, tail, 4, 13);
     print(head);
+
+    InsertAtPosition(head, tail, 1, 5);
+    print(head);
+
 
     return 0;
 }
